de7bf53fdf59f25c530cfdea909ec797ad1ce76c
cXiaof <310257509@qq.com>
2019-05-09 19:44:31 +0800

    use lodash.* instead of lodash; Upgrade dependency packages

--- dist/maptalks.polygonbool.es.js
+++ dist/maptalks.polygonbool.es.js
@@ -3,12 +3,6 @@
  * LICENSE : MIT
  * (c) 2016-2019 maptalks.org
  */
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.maptalks = global.maptalks || {})));
-}(this, (function (exports) { 'use strict';
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck$1(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -4156,10 +4150,6 @@ var PolygonBool = function (_maptalks$Class) {
 
 PolygonBool.mergeOptions(options);
 
-exports.PolygonBool = PolygonBool;
-
-Object.defineProperty(exports, '__esModule', { value: true });
+export { PolygonBool };
 
 typeof console !== 'undefined' && console.log('maptalks.polygonbool v0.1.0-beta.1');
-
-})));
